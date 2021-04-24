23e5a4e35a5f5ff7e2f28e7bf7c023124a496b74
Ruidy <r.nemausat@empfohlen.de>
2021-04-24 17:17:30 +0200

    overwrite entry point

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 #![no_std]
-
+#![no_main]
 use core::panic::PanicInfo;
 
-fn main() {}
+/// Disabling mangling ensures the compiler returns a function named `_start`
+///
+/// We tell the compiler to use `C calling convention`
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
 /// Function called on panic. It is a diverging function with a `never` return type.
 ///
 /// # Arguments
