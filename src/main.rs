9a90151756cb837e6c8205187ac8861c6e49ee8b
Ruidy <r.nemausat@empfohlen.de>
2021-04-24 17:03:58 +0200

    panic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+
+use core::panic::PanicInfo;
+
+fn main() {}
+/// Function called on panic. It is a diverging function with a `never` return type.
+///
+/// # Arguments
+/// - `info`- contains the file and line where the panic happened and the optional panic message
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
